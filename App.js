58a3dfaa7d2519de4cdc1539d00d63558453dbc3
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2020-07-19 23:55:16 +0900

    cacheImages

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { Asset } from "expo-asset";
+
+const cacheImages = (images) => images.map(image => {
+  if(typeof images === 'string'){
+    return Image.prefetch(image); // 배열 요소가 url 일때
+  }
+  else{
+    return Asset.fromModule(image).downloadAsync(); 
+  }
+});
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = async () => {
+    const images = cacheImages([
+      "https://images.unsplash.com/photo-1595091967394-ba11c2ead42f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+      require("./assets/icon.png")
+    ]);
+    console.log(images);
+  };
+  const onFinish = () => setIsReady(true);
   return (
-    <View style={styles.container}>
-      <Text>fuck you!</Text>
-      <StatusBar style="auto" />
-    </View>
+    isReady 
+    ? (<Text>Ready...</Text>) 
+    : (<AppLoading
+        startAsync={loadAssets} // 시작할 때 자동 수행
+        onFinish={onFinish} // 자동 수행된 함수가 완료
+        onError={console.error} // e => console.error(e)
+      />)
   );
 }
 
