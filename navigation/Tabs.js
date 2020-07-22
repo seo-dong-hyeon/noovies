59769366a7a31cda0ba4fda411e8a62b92e00621
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2020-07-23 03:48:41 +0900

    navagation & route -> header title update

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Movies from '../screens/Movies';
 import Tv from '../screens/Tv';
@@ -7,11 +7,21 @@ import Favs from '../screens/Favs';
 
 const Tabs = createBottomTabNavigator();
 
-export default () => (
-    <Tabs.Navigator>
-        <Tabs.Screen name="Movies" component={Movies} />
-        <Tabs.Screen name="Tv" component={Tv} />
-        <Tabs.Screen name="Search" component={Search} />
-        <Tabs.Screen name="Favs" component={Favs} />
-    </Tabs.Navigator>
-)
+// 처음 app 로드 -> routeName 없음(undefined) -> optional chaining, default 값
+const getHeaderName = route =>
+  route?.state?.routeNames[route.state.index] || "Movies";
+
+export default ({ navigation, route }) => { // 기본 인자 -> 부모 component랑 소통, 현재 Screen의 상태 정보
+    useLayoutEffect(() => { // useEffect와 비슷 -> layout이 rendering 된 이후에 실행
+        navigation.setOptions({title: getHeaderName(route)});
+    }, [route]); // 다른 tab으로 갈 때마다 route(해당 페이지 정보) 정보가 바뀜
+    return (
+        <Tabs.Navigator>
+            <Tabs.Screen name="Movies" component={Movies} />
+            <Tabs.Screen name="Tv" component={Tv} />
+            <Tabs.Screen name="Search" component={Search} />
+            <Tabs.Screen name="Favorate" component={Favs} />
+        </Tabs.Navigator>
+    );
+}
+    
--- screens/Movies.js
+++ screens/Movies.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
 export default ({ navigation }) => (
   <View>
     <Text>Movies</Text>
-    <Button title="Movie" onPress={() => navigation.navigate("Detail")}/>
+    <Button title="Movie" onPress={() => navigation.navigate("Detail")} /> 
   </View>
 );
